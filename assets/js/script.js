4786757dced8344adba846e22d8a7f0659ccd98e
Sadie Sial <sadiesial@gmail.com>
2021-11-27 23:41:43 -0600

    remove menu show from mobile resp. fx

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,16 @@ scrollUp.addEventListener("click", () => {
 const menu = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
+const navLink = document.querySelectorAll(".nav-link");
 
+// menu show
 menu.addEventListener("click", () => {
   ul.classList.toggle("show");
 });
+
+// menu hide
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
